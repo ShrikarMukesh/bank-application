9aedd9835e409a8da83d36623fd032fcb673d324
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-09-06 08:01:22 +0530

    Completed Config server implimentation

--- bank-application/accounts/src/main/java/com/accounts/AccountsApplication.java
+++ bank-application/accounts/src/main/java/com/accounts/AccountsApplication.java
@@ -3,12 +3,15 @@ package com.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScans({ @ComponentScan("com.accounts.controller") })
+@RefreshScope
+@ComponentScans({ @ComponentScan("com.accounts.controller") ,
+		@ComponentScan("com.accounts.config") })
 @EnableJpaRepositories("com.accounts.repository")
 @EntityScan("com.accounts.model")
 public class AccountsApplication {
--- bank-application/accounts/src/main/java/com/accounts/controller/AccountsController.java
+++ bank-application/accounts/src/main/java/com/accounts/controller/AccountsController.java
@@ -1,19 +1,30 @@
 package com.accounts.controller;
 
+import com.accounts.config.AccountsServiceConfig;
+import com.accounts.model.Accounts;
+import com.accounts.model.Customer;
+import com.accounts.model.Properties;
 import com.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.accounts.model.Accounts;
-import com.accounts.model.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+
 
 @RestController
 public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
+
+	@Autowired
+	AccountsServiceConfig accountsConfig;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -25,6 +36,15 @@ public class AccountsController {
 			return null;
 		}
 
+	}
+
+	@GetMapping("/account/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
